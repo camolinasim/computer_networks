e865d6d014ba272cef507088f4fab7b94a29f72a
Cristian Molina <cmutep@gmail.com>
2020-10-25 20:05:34 -0600

    now this works

--- filter.py
+++ filter.py
@@ -2,11 +2,17 @@
 from scapy.all import *
 import sys
 
-# Run example: sudo filter.py [your_interface], [filters (only icmp or tcp)]
+# Run example: sudo python3 filter.py [filters (only icmp or tcp)]
 # Run example: sudo filter.py ens33 icmp
+filters = sys.argv[1]
 
-interface = sys.argv[1]
-filters = sys.argv[2]
+if(str(filters) == 'icmp'):
+    sniff(count=5, filter=str(
+        filters), prn=lambda x: x.summary())
 
-sniff(iface=str(interface), count=5, filter=str(
-    filters), prn=lambda x: x.summary())
+elif(filters == 'tcp'):
+    port = input("enter a port number to listen...\n")
+    tcp_filter = 'tcp and port ' + str(port)
+    sniff(count=5, filter=tcp_filter, prn=lambda x: x.summary())
+else:
+    raise Exception("for filters, please choose icmp or tcp")
